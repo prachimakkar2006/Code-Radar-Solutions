fc7c8c2ee2942ab424cd82711b3b714b27b16c95
prachimakkar2006 <prachimakkar2006@gmail.com>
2025-02-04 03:16:28 +0530

    Solution to Check Number Sign

--- Check Number Sign@@6788dd63490637cfc44e1b9c/code.c
+++ Check Number Sign@@6788dd63490637cfc44e1b9c/code.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 
 int main() {
-   int x;
-   scanf("%d",&x);
-   if(x>0){  
-   printf("Positive");
-   }
-   else if (x<0){
-    pritnf("Negative");
-   }
-   else{
-    printf("Zero");
-   }
- return 0;
+    int x;
+    scanf("%d",&x);
+    if(x>o){
+        printf("Positive");
+    }
+    else if(x<0){
+        printf("Negative");
+    }
+    else{
+        pritnf("Zero");
+    }
 }
+
+  
