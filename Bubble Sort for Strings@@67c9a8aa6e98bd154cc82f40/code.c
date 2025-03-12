bb35b70e561f57a884c69d66263a4199495c11e3
prachimakkar2006 <prachimakkar2006@gmail.com>
2025-03-12 21:38:50 +0530

    Solution to Bubble Sort for Strings

---
+++ Bubble Sort for Strings@@67c9a8aa6e98bd154cc82f40/code.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+
+int normalizeShift(int shift) {
+    shift = shift % 26;    
+    if (shift < 0) shift += 26; 
+    return shift;
+}
+
+
+void caesarCipher(char message[], int shift, char encrypted[]) {
+    for (int i = 0; message[i] != '\0'; i++) {
+        char ch = message[i];
+
+        if (ch >= 'a' && ch <= 'z') { 
+            encrypted[i] = ((ch - 'a' + shift) % 26) + 'a';
+        } else if (ch >= 'A' && ch <= 'Z') { 
+            encrypted[i] = ((ch - 'A' + shift) % 26) + 'A';
+        } else { 
+            encrypted[i] = ch;
+        }
+    }
+    encrypted[strlen(message)] = '\0'; 
+}
+
