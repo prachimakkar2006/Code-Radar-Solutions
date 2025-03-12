ac72c32a4ad05acf96249d29cd2f6323231ea9b7
prachimakkar2006 <prachimakkar2006@gmail.com>
2025-03-12 18:58:47 +0530

    Solution to Smallest Window Sort

---
+++ Smallest Window Sort@@67ca7acd1fe08d7207c253b5/code.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+int findUnsortedSubarray(int arr[], int n) {
+    int start = -1, end = -1;
+    for(int i = 0; i < n - 1; i++) {
+        if(arr[i] > arr[i + 1]) {
+            start = i;
+            break;
+        }
+    }
+    if(start == -1) return 0;
+
+    
+    for(int i = n - 1; i > 0; i--) {
+        if(arr[i] < arr[i - 1]) {
+            end = i;
+            break;
+        }
+    }
+
+    int min = arr[start], max = arr[start];
+    for(int i = start; i <= end; i++) {
+        if(arr[i] < min) min = arr[i];
+        if(arr[i] > max) max = arr[i];
+    }
+    for(int i = 0; i < start; i++) {
+        if(arr[i] > min) {
+            start = i;
+            break;
+        }
+    }
+
+
+    for(int i = n - 1; i > end; i--) {
+        if(arr[i] < max) {
+            end = i;
+            break;
+        }
+    }
+
+    return end - start + 1;
+}
+
+int main() {
+    int t;
+    scanf("%d", &t);
+
+    while(t--) {
+        int n;
+        scanf("%d", &n);
+
+        int arr[10000];
+        for(int i = 0; i < n; i++) {
+            scanf("%d", &arr[i]);
+        }
+
+        printf("%d\n", findUnsortedSubarray(arr, n));
+    }
+
+    return 0;
+}
