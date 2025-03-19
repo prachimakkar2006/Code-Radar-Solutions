47986996b65813b8613356f81af486524564bab5
prachimakkar2006 <prachimakkar2006@gmail.com>
2025-03-19 10:56:56 +0530

    Solution to Find the Maximum and Minimum Element in an Array

--- Find the Maximum and Minimum Element in an Array@@676c02002dbc8c3e7a280b70/code.c
+++ Find the Maximum and Minimum Element in an Array@@676c02002dbc8c3e7a280b70/code.c
@@ -1,17 +1,18 @@
 #include<stdio.h>
-int main(){ 
+int main(){
     int n;
     scanf("%d",&n);
     int arr[n];
     for(int i=0;i<n;i++){
-        scanf("%d",arr[i]);
+        scanf("%d ",&arr[i]);
     }
-    int num=0;
+    int num=arr[0];
+    int num2=arr[0];
     for(int i=0;i<n;i++){
         if(arr[i]>num){
             num=arr[i];
         }
     }
-    printf("%d",num);
     return 0;
+    
 }
