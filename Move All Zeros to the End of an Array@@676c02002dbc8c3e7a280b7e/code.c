3a6edf443790616a95c74a8d193e88c8827eeae9
prachimakkar2006 <prachimakkar2006@gmail.com>
2025-03-30 02:06:47 +0530

    Solution to Move All Zeros to the End of an Array

--- Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
+++ Move All Zeros to the End of an Array@@676c02002dbc8c3e7a280b7e/code.c
@@ -1,27 +1,40 @@
-#include<stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0;i<n;i++){
-        scanf("%d",&arr[i]);  
-    }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n-i-1;j++){
-            if(arr[j]>arr[j+1]){
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+#include <stdio.h>
 
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+
+    // Input elements
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    // Bubble Sort
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
-            
         }
     }
-    for(int i=0;i<n;i++){
-        if(arr[i]==0){
-            arr[i]=arr[n];  
+
+    // Print non-zero elements first
+    int zero_count = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            printf("%d ", arr[i]);
+        } else {
+            zero_count++;
         }
-    printf("%d ",arr[i]);
     }
-    
+
+    // Print all zeros at the end
+    for (int i = 0; i < zero_count; i++) {
+        printf("0 ");
+    }
+
+    return 0;
 }
