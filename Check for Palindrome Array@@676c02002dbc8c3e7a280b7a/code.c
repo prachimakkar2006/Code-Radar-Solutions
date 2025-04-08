ec90bdf5352fe67bf86e8ef9b5681beb429ab178
prachimakkar2006 <prachimakkar2006@gmail.com>
2025-04-08 15:15:56 +0530

    Solution to Check for Palindrome Array

--- Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
+++ Check for Palindrome Array@@676c02002dbc8c3e7a280b7a/code.c
@@ -10,16 +10,16 @@ int main() {
     }
 
     for(int i = 0; i < n / 2; i++) {
-        if(arr[i] != arr[n - i - 1]) {
+        if(arr[i] == arr[n - i - 1]) {
             isPalindrome = 1;
             break;
         }
     }
 
     if(isPalindrome>0)
-        printf("NO");
-    else
         printf("YES");
+    else
+        printf("NO");
 
     return 0;
 }
